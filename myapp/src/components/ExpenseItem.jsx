49046496818393050b1efdb252bb53a6ff73d60a
Manish Pandey <mnish.libra@gmail.com>
2023-07-07 11:19:37 +0530

    Assignment

---
+++ myapp/src/components/ExpenseItem.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ExpenseItem = () => {
+    return (
+        <div>
+            <h1>Expense Item !</h1>
+            <h6>Food Rs 10</h6>
+            <h6>Petrol Rs 100</h6>
+            <h6>Movies Rs 200</h6>
+        </div>
+    )
+} 
+
+export default ExpenseItem;
